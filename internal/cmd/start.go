f186127b302655b5fa82c374119ad43a5d98ac12
Janusz Piechówka <januszpiechowka@gmail.com>
2020-10-25 15:11:36 +0100

    Add ability to configure Basic Auth

--- internal/cmd/start.go
+++ internal/cmd/start.go
@@ -1,9 +1,11 @@
 package cmd
 
 import (
+	"errors"
 	"github.com/jpiechowka/go-file-server/internal/config"
 	"github.com/jpiechowka/go-file-server/internal/server"
 	"github.com/spf13/cobra"
+	"strings"
 )
 
 const (
@@ -12,12 +14,10 @@ const (
 )
 
 var (
-	serverAddr         string
-	serveDirectoryPath string
+	serverAddr           string
+	serveDirectoryPath   string
+	basicAuthCredentials string
 	//enableTls         bool
-	//enableBasicAuth   bool
-	//basicAuthUser     string
-	//basicAuthPassword string
 
 	startCommand = &cobra.Command{
 		Use:   "start",
@@ -35,12 +35,30 @@ var (
 func init() {
 	startCommand.Flags().StringVarP(&serverAddr, "address", "a", defaultServerAddr, "Server address")
 	startCommand.Flags().StringVarP(&serveDirectoryPath, "dir", "d", defaultServeDirectoryPath, "Path to directory with files to serve")
+	startCommand.Flags().StringVarP(&basicAuthCredentials, "basic-auth", "b", "", "Enables Basic Auth. Credentials should be provided as username:password")
 }
 
 func startCmd() error {
 	cfg := &config.ServerConfig{
 		Address:            serverAddr,
 		ServeDirectoryPath: serveDirectoryPath,
+	}
+
+	if basicAuthCredentials != "" {
+		cfg.EnableBasicAuth = true
+
+		credentials := strings.Split(basicAuthCredentials, ":")
+
+		if len(credentials) != 2 {
+			return errors.New("provided Basic Auth credentials are invalid. Expected format is username:password")
+		}
+
+		if credentials[0] == "" || credentials[1] == "" {
+			return errors.New("provided Basic Auth credentials are invalid. Password and username cannot be empty")
+		}
+
+		cfg.BasicAuthUser = credentials[0]
+		cfg.BasicAuthPassword = credentials[1]
 	}
 
 	srv := server.NewFiberFileServer(cfg)
--- internal/server/routes.go
+++ internal/server/routes.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"github.com/gofiber/fiber/v2/middleware/basicauth"
 	"github.com/gofiber/fiber/v2/middleware/compress"
 	"github.com/gofiber/fiber/v2/middleware/filesystem"
 	"github.com/gofiber/fiber/v2/middleware/limiter"
@@ -14,20 +15,33 @@ import (
 func (s *FiberFileServer) setupRoutingAndMiddleware() {
 	// Middleware configuration
 	s.fiber.Use(recover.New()) // Default fiber panic recovery middleware
+
+	if s.config.EnableBasicAuth {
+		s.fiber.Use(basicauth.New(basicauth.Config{
+			Users: map[string]string{
+				s.config.BasicAuthUser: s.config.BasicAuthPassword,
+			},
+		}))
+	}
+
 	s.fiber.Use(logger.New(logger.Config{
 		Format:     "[${time}] ${status} - ${ip} ${latency} ${method} ${path}\n",
 		TimeFormat: "Mon Jan-_2 15:04:05 MST", // https://gobyexample.com/time-formatting-parsing
 		TimeZone:   "Local",
 	}))
+
 	s.fiber.Use(middleware.DisableCache())
+	s.fiber.Use(middleware.AddSecurityHeaders(false)) // TODO: HSTS config
+
 	s.fiber.Use(compress.New(compress.Config{
 		Level: compress.LevelBestCompression, // TODO: compression level or disable provided in config
 	}))
-	s.fiber.Use(middleware.AddSecurityHeaders(false)) // TODO: HSTS config
+
 	s.fiber.Use(limiter.New(limiter.Config{
 		Max:      60,
 		Duration: 1 * time.Minute,
 	}))
+
 	s.fiber.Use(filesystem.New(filesystem.Config{
 		Root:   http.Dir(s.config.ServeDirectoryPath),
 		Browse: true,
